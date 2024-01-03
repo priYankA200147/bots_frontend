8450c6bd61ee400b26355c9bb5b9877030296efb
Rakshanda-Pande <rakshandapande2107@gmail.com>
2024-01-03 15:52:47 +0530

    added base path in index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import BotUserLogin from './components/BotUserLogin';
-import { store } from './store';
-import BotPage from './components/BotPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BotUserLogin from "./components/BotUserLogin";
+import { store } from "./store";
+import BotPage from "./components/BotPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-  <Routes>
-  <Route path="/" element={<BotUserLogin/>}/>
-  <Route path="/bot-page" element={<BotPage/>}/>
-  </Routes>
+    <BrowserRouter basename="bots">
+      <Routes>
+        <Route path="/" element={<BotUserLogin />} />
+        <Route path="/bot-page" element={<BotPage />} />
+      </Routes>
     </BrowserRouter>
   </Provider>
 );
